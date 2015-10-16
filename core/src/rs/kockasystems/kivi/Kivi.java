2c34aa696b196d0dfa0dad2969a6011dd95c07df
KockaAdmiralac <luka.kocka@gmail.com>
2015-10-16 17:00:35 +0200

    Added map mode.

--- core/src/rs/kockasystems/kivi/Kivi.java
+++ core/src/rs/kockasystems/kivi/Kivi.java
@@ -1,5 +1,7 @@
 package rs.kockasystems.kivi;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import com.badlogic.gdx.ApplicationAdapter;
@@ -8,10 +10,18 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.objects.PolylineMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Vector2;
 
 public class Kivi extends ApplicationAdapter implements InputProcessor
 {
@@ -22,11 +32,17 @@ public class Kivi extends ApplicationAdapter implements InputProcessor
 	private BitmapFont font;
 	private final Random rnd = new Random();
 	private Music music;
-	private final float ACCELERATION = 0.1f;
+	private final float ACCELERATION = 0.001f;
 	private byte mode;
 	private float soundVolume;
 	private boolean enableParticles;
 	private float label1, label2;
+	private List<Effect> effects;
+	private OrthographicCamera camera;
+	private TiledMap map;
+	private TiledMapRenderer renderer;
+	private TmxMapLoader loader;
+	private float[] vertices;
 	
 	// Game Modes
 	private final byte MODE_CLASSIC 		= 0;
@@ -35,19 +51,31 @@ public class Kivi extends ApplicationAdapter implements InputProcessor
 	private final byte MODE_ACCURATE 		= 3;
 	private final byte MODE_ACCURATE_FAST 	= 4;
 	private final byte MODE_WALLS 			= 5;
+	private final byte MODE_MAP				= 6;
 	
 	@Override
 	public void create ()
 	{
 		batch = new SpriteBatch();
 		kiviTexture = new Texture("Ball.png");
-		ball = new KiviBall((rnd.nextFloat() - 0.5f) * 10, (rnd.nextFloat() - 0.5f) * 10);
+		ball = new KiviBall((rnd.nextFloat() - 0.5f) * 5, (rnd.nextFloat() - 0.5f) * 5);
 		font = new BitmapFont();
+		effects = new ArrayList<Effect>();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.update();
+		loader = new TmxMapLoader();
+		map = loader.load("maps/2.tmx");
+		vertices = ((PolylineMapObject)map.getLayers().get("Objects").getObjects().get(0)).getPolyline().getTransformedVertices();
+		renderer = new OrthogonalTiledMapRenderer(map);
+		
+		soundVolume = 1f;
+		enableParticles = true;
 		
 		music = Gdx.audio.newMusic(Gdx.files.internal("music.ogg"));
 		//music.play();
 		
-		mode = MODE_WALLS;
+		mode = MODE_MAP;
 		
 		label1 = 0;
 		label2 = 30;
@@ -60,14 +88,34 @@ public class Kivi extends ApplicationAdapter implements InputProcessor
 	@Override
 	public void render ()
 	{
-		if(mode == MODE_CLASSIC)label2 += Gdx.graphics.getDeltaTime();
+		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        if(mode == MODE_CLASSIC)label2 += Gdx.graphics.getDeltaTime();
 		else if(mode == MODE_TIMED || mode == MODE_CHALLENGE || mode == MODE_ACCURATE_FAST)label2 -= Gdx.graphics.getDeltaTime();
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
 		ball.update();
+		camera.update();
+        renderer.setView(camera);
+        renderer.render();
+        
 		batch.begin();
+			
 			ball.draw(batch);
 			font.draw(batch, String.valueOf((int)label1), Gdx.graphics.getWidth() / 2 - 10, Gdx.graphics.getHeight() / 10);
 			font.draw(batch, String.valueOf((int)label2), Gdx.graphics.getWidth() / 2 - 10, Gdx.graphics.getHeight() / 10 + 20);
+			if((enableParticles || soundVolume > 0) && effects.size() > 0)for(int i=0; i<effects.size(); ++i)
+			{
+				if(effects.get(i).started())effects.get(i).update(batch, Gdx.graphics.getDeltaTime());
+				else if(effects.get(i).stopped())
+				{
+					effects.get(i).stop();
+					effects.remove(i);
+				}
+			}
+			
 		batch.end();
 		if(((mode == MODE_CLASSIC || mode == MODE_CHALLENGE) && label1 == 0) || ((mode == MODE_TIMED || mode == MODE_CHALLENGE || mode == MODE_ACCURATE || mode == MODE_ACCURATE_FAST || mode == MODE_WALLS) && (int)label2 == 0))
 		{
@@ -90,60 +138,86 @@ public class Kivi extends ApplicationAdapter implements InputProcessor
 		
 		public void draw(SpriteBatch batch)
 		{
-			batch.draw(kiviTexture, x - (kiviTexture.getWidth() / 2), y - (kiviTexture.getHeight() / 2));
+			batch.draw(kiviTexture, left(), top());
 		}
 		
 		public void update()
 		{
 			x += vx;
 			y += vy;
-			if(x + (kiviTexture.getWidth() / 2) >= Gdx.graphics.getWidth() || x - (kiviTexture.getWidth() / 2) < 0)
+			camera.translate(vx, vy);
+			
+			final boolean COLLIDING_LEFT 	= mode == MODE_MAP ? isColliding() 	: left() < 0;
+			final boolean COLLIDING_RIGHT 	= mode == MODE_MAP ? isColliding() 	: right() >= Gdx.graphics.getWidth();
+			final boolean COLLIDING_TOP		= mode == MODE_MAP ? isColliding() 	: top() >= Gdx.graphics.getWidth();
+			final boolean COLLIDING_BOTTOM	= mode == MODE_MAP ? isColliding() 	: bottom() < 0;
+			if(COLLIDING_LEFT || COLLIDING_RIGHT)
 			{
+				if(enableParticles || soundVolume > 0)effects.add(new Effect("bounce", (COLLIDING_LEFT ? left() : right()), y));
 				vx = -vx;
 				if(mode == MODE_WALLS)label2--;
 			}
-			if(y + (kiviTexture.getHeight() / 2) >= Gdx.graphics.getHeight() || y - (kiviTexture.getHeight() / 2) < 0){
+			if(COLLIDING_TOP || COLLIDING_BOTTOM)
+			{
+				if(enableParticles || soundVolume > 0)effects.add(new Effect("bounce", x, (COLLIDING_BOTTOM ? bottom() : top() )));
 				vy = -vy;
 				if(mode == MODE_WALLS)label2--;
 			}
 		}
 		
+		private boolean isColliding()
+		{
+		    for (int i = 0; i < vertices.length; i += 2)
+		    {
+		        if (i == 0){ if (Intersector.intersectSegmentCircle(new Vector2(vertices[vertices.length-2], vertices[vertices.length-1]), new Vector2(vertices[i] / 2, vertices[i + 1] / 2), new Vector2(x, y), (kiviTexture.getWidth() / 2) * (kiviTexture.getWidth() / 2)))return true; }
+		        else if (Intersector.intersectSegmentCircle(new Vector2(vertices[i - 2], vertices[i - 1]), new Vector2(vertices[i], vertices[i + 1]), new Vector2(x, y), (kiviTexture.getWidth() / 2) * (kiviTexture.getWidth() / 2))){
+		        	System.out.println(new Vector2(vertices[i - 2], 800 - vertices[i - 1]) + " " + new Vector2(vertices[i], 800 - vertices[i + 1]) + " " + new Vector2(x, y));
+		        	return true;
+		        }
+		    }
+			return false;
+		}
+		
+		private float top(){ return y - (kiviTexture.getHeight() / 2); }
+		private float bottom(){ return y + (kiviTexture.getHeight() / 2); }
+		private float left(){ return x - (kiviTexture.getWidth() / 2); }
+		private float right(){ return x + (kiviTexture.getWidth() / 2); }
+		
 	}
 	
 	class Effect
 	{
-		private String name;
-		private float x, y;
 		private ParticleEffect effect;
 		private Sound sound;
+		private boolean started;
 		
 		public Effect(final String name, final float x, final float y)
 		{
-			this.name = name;
-			this.x = x;
-			this.y = y;
 			sound = Gdx.audio.newSound(Gdx.files.internal("effects/" + name + "/" + name + ".wav"));
 			effect = new ParticleEffect();
+			effect.load(Gdx.files.internal("effects/" + name + "/" + name + ".particle"), Gdx.files.internal("images"));
 			effect.setPosition(x, y);
+			started = true;
+			if(soundVolume > 0) sound.setVolume(sound.play(), soundVolume);
+			if(enableParticles)effect.start();
 		}
 		
-		public void play()
+		public void update(SpriteBatch batch, float delta)
 		{
-			if(soundVolume > 0)sound.play();
-			effect.start();
+			started = !effect.isComplete();
+			effect.draw(batch, delta);
 		}
+		
+		public void stop()
+		{
+			effect.dispose();
+			sound.dispose();
+		}
+		
+		public boolean started() { return started; }
+		public boolean stopped() { return effect.isComplete(); }
 	}
 	
-	class EffectsThread extends Thread
-	{
-		public void run()
-		{
-			while(true)
-			{
-				
-			}
-		}
-	}
 	
 	
 	// Unused
@@ -168,6 +242,7 @@ public class Kivi extends ApplicationAdapter implements InputProcessor
 		if(mode == MODE_ACCURATE || mode == MODE_ACCURATE_FAST)label1--;
 		if(screenX > ball.x - (kiviTexture.getWidth() / 2) && screenX < ball.x + (kiviTexture.getWidth() / 2) && Gdx.graphics.getHeight() - screenY > ball.y - (kiviTexture.getHeight() / 2) && Gdx.graphics.getHeight() - screenY < ball.y + (kiviTexture.getHeight() / 2))if(dist(screenX, Gdx.graphics.getHeight() - screenY, ball.x, ball.y) <= kiviTexture.getHeight() / 2)
 		{
+			if(enableParticles)effects.add(new Effect("click", ball.x, ball.y));
 			if(mode == MODE_CLASSIC || mode == MODE_CHALLENGE)label1--;
 			else if ((mode == MODE_TIMED || mode == MODE_ACCURATE || mode == MODE_WALLS))label1++;
 			else if (mode == MODE_ACCURATE_FAST)label1 += 3;
